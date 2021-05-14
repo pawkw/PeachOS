30528c271797a581178c85fd600908dd9d551061
pawkw <pawkw@no-replay.users.github.com>
2021-05-14 01:01:28 -0400

    Update display

--- src/kernel.c
+++ src/kernel.c
@@ -1,32 +1,45 @@
 #include "kernel.h"
 
+uint16_t* video_mem = 0;
+
 int len(char string[]) {
     int count = 0;
     while(string[count] != '\0') ++count;
     return count;
 }
 
-void display(char* screen, char string[] , int line, int col, int fg, int bg) {
+uint16_t terminal_make_char(char c, char colour) {
+    return (colour << 8) | c;
+}
+
+char terminal_make_colour(char fg, char bg) {
+    return (bg << 3) | fg;
+}
+
+void display(uint16_t* screen, char string[] , int line, int col, char fg, char bg) {
     int length = len(string);
-    int start = line * 80 + (col<<1);
+    int start = line * VGA_WIDTH + col;
     for(int i = 0; i < length; i++) {
-        screen[start + (i<<1)] = string[i];
-        screen[start + (i<<1) + 1] = (bg << 3) + fg;
+        screen[start + i] = terminal_make_char(string[i], terminal_make_colour(fg, bg));
     }
 }
 
-void cls(char* screen, char character, int colour) {
-    for(int i = 0; i < 2000; i++) {
-        screen[i<<1] = character;
-        screen[(i<<1)+1] = (colour<<4);
-
+void terminal_initialize() {
+    video_mem = (uint16_t*)0xb8000;
+    uint16_t blank = terminal_make_char(' ', terminal_make_colour(8, 15));
+    for(int y = 0; y < VGA_HEIGHT; y++) {
+        for (int x = 0; x < VGA_WIDTH; x++)
+        {
+            video_mem[(y*VGA_WIDTH)+x] = blank;
+        }
+        
     }
+
 }
 
 void kernel_main() {
-    char* video_mem = (char*)0xb8000;
     char string[] = "Welcome to Agree JOS.\0";
     
-    cls(video_mem, ' ', 1);
-    display(video_mem, string, 10, 40 - (len(string)>>1), 15, 1);
+    terminal_initialize();
+    display(video_mem, string, 10, 40 - (len(string)>>1), 8, 15);
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,6 +1,11 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+#include <stdint.h>
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
 void kernel_main();
 
 #endif
